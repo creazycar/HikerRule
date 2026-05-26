5ae9f8f760d6fa0abcd12c0a97a89bc8aea71f3e
creazycar <50282965+creazycar@users.noreply.github.com>
2026-05-26 16:09:44 +0800

    Update hikermovie.js

--- hikermovie.js
+++ hikermovie.js
@@ -3021,7 +3021,7 @@ function omerj() {
         let check = fetch("hiker://home@XYQ推送");
         if (!check || check.length <= 5) {
             let ru = $.toString(() => {
-                return "海阔视界，XYQ推送￥home_rule_url￥https://github.moeyy.xyz/https://raw.githubusercontent.com/xyq254245/HikerRule/main/XYQ%e6%8e%a8%e9%80%81.json"
+                return "海阔视界，XYQ推送￥home_rule_url￥https://github.moeyy.xyz/https://raw.githubusercontent.com/creazycar/HikerRule/refs/heads/main/XYQ%E6%8E%A8%E9%80%81.json"
             });
             confirm({
                 title: MY_RULE.title + '提示',
